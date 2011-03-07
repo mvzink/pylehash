0f0c0750fd4de48463b80ce8d5ba3197dfaca39f
Michael Victor Zink <zuwiki@zuwiki.net>
2011-03-07 00:10:37 -0600

    Moving stuff from server.py into pylehash/__init__.py

--- pylehash/__init__.py
+++ pylehash/__init__.py
@@ -11,3 +11,33 @@ from .switch import Switch
 from .end import End, EndManager
 import hash
 import handlers
+
+
+def make_switch(port, ip=None, seed=None):
+    if ip:
+        s = Switch(ipp = (ip[0], port))
+    elif seed:
+        s = Switch(seed_ipp=(seed[0], int(seed[1])))
+    else:
+        s = Switch(seed_ipp=('208.68.163.247', 42424))
+    return s
+
+def run_switch(port, switch):
+    from . import reactor
+    reactor.listenUDP(port, switch)
+    reactor.run()
+
+def get_switch_from_args():
+    import argparse
+    parser = argparse.ArgumentParser(description='Start a pylehash server')
+    parser.add_argument('port', nargs='?', type=int, default=0,
+        help='The port number to listen on (default: random)')
+    parser.add_argument('--seed', nargs=2, dest='seed', metavar=('HOST', 'PORT'), default=False,
+        help='The port and host of a seed to contact. \
+            (default: 208.68.163.247:42424 aka telehash.org:42424)')
+    parser.add_argument('--no-seed', nargs=1, dest='ip', metavar='HOST', default=False,
+        help='Don\'t contact a seed; usually means we will be a seed for a network. Argument specifies our own IP address. \
+            Overrides --seed (default: no)')
+    args = parser.parse_args()
+
+    return (args.port, make_switch(args.port, seed=args.seed, ip=args.ip))
--- server.py
+++ server.py
@@ -1,36 +1,7 @@
 #!/usr/bin/env python
-'''
-Created on Feb 3, 2011
 
-@author: Michael Victor Zink <zuwiki@zuwiki.net>
-'''
-
-from pylehash import Switch, reactor
-
-def make_switch(port, ip=None, seed=None):
-    if ip:
-        s = Switch(ipp = (ip[0], port))
-    elif seed:
-        s = Switch(seed_ipp=(seed[0], int(seed[1])))
-    else:
-        s = Switch(seed_ipp=('208.68.163.247', 42424))
-    return s
-
-def run(port, switch):
-    reactor.listenUDP(port, switch)
-    reactor.run()
+from pylehash import get_switch_from_args, run_switch
 
 if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser(description='Start a pylehash server')
-    parser.add_argument('port', nargs='?', type=int, default=0,
-        help='The port number to listen on (default: random)')
-    parser.add_argument('--seed', nargs=2, dest='seed', metavar=('HOST', 'PORT'), default=False,
-        help='The port and host of a seed to contact. \
-            (default: 208.68.163.247:42424 aka telehash.org:42424)')
-    parser.add_argument('--no-seed', nargs=1, dest='ip', metavar='HOST', default=False,
-        help='Don\'t contact a seed; usually means we will be a seed for a network. Argument specifies our own IP address. \
-            Overrides --seed (default: no)')
-    args = parser.parse_args()
-
-    run(args.port, make_switch(args.port, seed=args.seed, ip=args.ip))
+    port, switch = get_switch_from_args()
+    run_switch(port, switch)
